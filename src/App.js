e8f06ceb176680219ef5c290a2b9a48912f78de3
arcferrer5 <arcferrer5@gmail.com>
2023-01-25 21:00:20 +0800

    Added general structure and layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <main>
+      <div className="header-container">
+        <span className='logo1'>avionbank</span>
+        <span className='logo2'>logo here</span>
+      </div>
+
+      <div className="main-dash">
+        <div className="money-container">
+          <h1 className="balance">$0.00</h1>
+          <h4 className="acct-number">6353 7863 5274 9817</h4>
+          <div className='money-footer'>
+            <p className="date">01/26</p>
+            <span className="card-type">VISA</span>
+          </div>
+        </div>
+
+        <div className="action-container">
+          <div className="action-buttons deposit">
+            <FontAwesomeIcon icon={faEnvelope} />
+            <span>Deposit</span>
+          </div>
+          <div className="action-buttons send-money">
+            <FontAwesomeIcon icon={faEnvelope} />
+            <span>Send Money</span>
+          </div>
+          <div className="action-buttons withdraw">
+            <FontAwesomeIcon icon={faEnvelope} />
+            <span>Withdraw</span>
+          </div>
+          <div className="action-buttons friends">
+            <FontAwesomeIcon icon={faEnvelope} />
+            <span>Friends</span>
+          </div>
+        </div>
+      </div>
+    </main>
+    </>
   );
 }
 
