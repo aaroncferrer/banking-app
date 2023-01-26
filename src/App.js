9cc722162ede66f763f2df47f49d10ac0d304338
arcferrer5 <arcferrer5@gmail.com>
2023-01-26 21:04:33 +0800

    Semi-finalized bank dashboard

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faDollarSign } from '@fortawesome/free-solid-svg-icons'
+import { faUserFriends } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
     <>
     <main>
       <div className="header-container">
-        <span className='logo1'>avionbank</span>
-        <span className='logo2'>logo here</span>
+        <img src='./bank-logo.png' alt='Bank Logo' className='logo1' />
+        {/* <span className='logo2'>logo here</span> */}
       </div>
 
       <div className="main-dash">
+      
         <div className="money-container">
           <h1 className="balance">$0.00</h1>
           <h4 className="acct-number">6353 7863 5274 9817</h4>
@@ -23,22 +25,26 @@ function App() {
 
         <div className="action-container">
           <div className="action-buttons deposit">
-            <FontAwesomeIcon icon={faEnvelope} />
-            <span>Deposit</span>
+            <span className="action-symbol">+</span>
+            <FontAwesomeIcon className='action-logo' icon={faDollarSign} />
+            <span className='action-name'>Deposit</span>
           </div>
           <div className="action-buttons send-money">
-            <FontAwesomeIcon icon={faEnvelope} />
-            <span>Send Money</span>
+            <span className="action-symbol">▶</span>
+            <FontAwesomeIcon className='action-logo' icon={faDollarSign} />
+            <span className='action-name'>Send Money</span>
           </div>
           <div className="action-buttons withdraw">
-            <FontAwesomeIcon icon={faEnvelope} />
-            <span>Withdraw</span>
+            <span className="action-symbol">─</span>
+            <FontAwesomeIcon className='action-logo' icon={faDollarSign} />
+            <span className='action-name'>Withdraw</span>
           </div>
           <div className="action-buttons friends">
-            <FontAwesomeIcon icon={faEnvelope} />
-            <span>Friends</span>
+            <FontAwesomeIcon className='action-logo' icon={faUserFriends} />
+            <span className='action-name'>Friends</span>
           </div>
         </div>
+
       </div>
     </main>
     </>
